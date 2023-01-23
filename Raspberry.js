602f85fc1394a84f103a88de3e2762c18d51d25d
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-01-23 15:48:11 +0100

    Add files via upload

--- Raspberry.js
+++ Raspberry.js
@@ -1,6 +1,20 @@
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline')
 
+var mysql = require('mysql');
+const { validateHeaderName } = require('http');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "centralepollution"
+});
+
+con.connect(function(err) {
+if (err) throw err;
+console.log("[+] Connected to database");
+});
+
 var AirQuality = 0;
 var CO2 = 0;
 var COV = 0;
@@ -15,6 +29,19 @@ const comPort1 = new SerialPort({
     parity: 'none',
 });
 
+function SendData(type, value)
+{
+    let valuestr = '';
+    for (var i = 0; i < value.toString().length; i++) {
+        if(value[i] != '\n' && value[i] != '\r' && value[i] != '\n\r')
+          valuestr+= value[i];
+    }
+    let sql = "INSERT INTO mesures (Capteur, Mesure) VALUES('" + type.toString() + "', '" + valuestr + "')";
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) console.log("[-] " + err);
+    });
+}
 
 comPort1.on('data', data => readSerialData(data.toString()));
 
@@ -30,31 +57,35 @@ function readSerialData(data) {
                 case 'airquality' :
                     if(Number(splitData[1].toString()) >= 0 && Number(splitData[1].toString()) <= 3){
                         AirQuality = Number(splitData[1].toString());
+                        SendData(splitData[0], splitData[1]);
                     }
                     break;
                 case 'CO2':
                     if(Number(splitData[1].toString()) >= 0 && Number(splitData[1].toString()) <= 20000){
                         CO2 = Number(splitData[1].toString());
+                        SendData(splitData[0], splitData[1]);
                     }
                     break;
                 case 'COV':
                     if(Number(splitData[1].toString()) >= 0.0 && Number(splitData[1].toString()) <= 50.0){
                         COV = parseFloat(splitData[1].toString());
+                        SendData(splitData[0], splitData[1]);
                     }
                     break;
                 case 'temperature':
                     if(parseFloat(splitData[1].toString()) >= 0.0 && parseFloat(splitData[1].toString()) <= 50.0){
                         Temp = parseFloat(splitData[1].toString());
+                        SendData(splitData[0], splitData[1]);
                     }
                     break;
             }
         } 
     } 
 
-    console.log("Air Quality : " + AirQuality.toString());
+    /*console.log("Air Quality : " + AirQuality.toString());
     console.log("CO2 : " + CO2.toString() + "ppm");
     console.log("COV : " + COV.toString() + "ppb");
     console.log("Temperature : " + Temp.toString() + "°C");
     console.log("====================================");
-   
+   */
 }

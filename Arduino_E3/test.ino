9f1a7fab9e3be0d6568c3d3539e5d86feec11d84
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-01-26 18:33:36 +0100

    là c'est souple en vrai non

--- Arduino_E3/test.ino
+++ Arduino_E3/test.ino
@@ -2,29 +2,46 @@
 #include "AirQualitySensor.h"
 #include "co2covSensor.h"
 
+#define USING_SOFTWARE_SERIAL //Retirer cette ligne pour debug avec le moniteur série
+
 // CAPTEURS UTILISES
 #define AIR_QUALITY_SENSOR
 
 // PINS
-
 #ifdef AIR_QUALITY_SENSOR
-#define AIR_QUALITY_SENSOR_PIN A0
+  #define AIR_QUALITY_SENSOR_PIN A0
 #endif
 
-Com *server;
+#ifdef USING_SOFTWARE_SERIAL
+  #define TX_PIN 10
+  #define RX_PIN 11
+  Communication* communication;
+#endif 
+
 AQSensor *airQualitySensor;
 Co2VocSensor *co2covSensor;
 
 void setup() {
-  Serial.begin(230400);
-  server = new Com();
+  
+  #ifdef USING_SOFTWARE_SERIAL
+    communication = new Communication(TX_PIN, RX_PIN, BAUD_RATE);
+  #else 
+    Serial.begin(BAUD_RATE)
+  #endif 
+      
   airQualitySensor = new AQSensor(AIR_QUALITY_SENSOR_PIN);
   co2covSensor = new Co2VocSensor();
 }
 
 void loop() {
   delay(500);
-  server->SendData("airquality", airQualitySensor->getValue());
-  server->SendData("CO2", co2covSensor->GetCO2());
-  server->SendData("COV", co2covSensor->GetCOV());
+  #ifdef USING_SOFTWARE_SERIAL
+    communication->SendData("airquality", airQualitySensor->getValue());
+    communication->SendData("CO2", co2covSensor->GetCO2());
+    communication->SendData("COV", co2covSensor->GetCOV());
+  #else
+    Serial.println("airquality: %d", );
+    Serial.println("CO2: %d", co2covSensor->GetCO2());
+    Serial.println("COV: %d", co2covSensor->GetCOV());
+  #endif
 }

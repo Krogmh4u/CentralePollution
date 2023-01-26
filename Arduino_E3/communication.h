048fd0181b3f1ba49f4bbb6b8666ca656d0d4e04
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-01-26 18:29:13 +0100

    HardwareSerial to SoftwareSerial

--- Arduino_E3/communication.h
+++ Arduino_E3/communication.h
@@ -1,14 +1,21 @@
-#include <WString.h>
-#include <Arduino.h>
-#include <HardwareSerial.h>
-
-class Com
+class Communication : public SoftwareSerial
 {
- public :
+  public :
+
+  Communication(const int TX_PIN, const int RX_PIN, const int BAUD_RATE = 9600) :
+    SoftwareSerial(RX_PIN, TX_PIN), 
+    TX_Pin(TX_PIN), RX_Pin(RX_PIN), BaudRate(BAUD_RATE)
+    {
+      begin(BaudRate);
+    }
+
     template<typename T> inline void SendData(const String name, T data) const noexcept
     {
       Serial.println(name + ":" + String(data));
     }
 
- private :
+  private : 
+
+  int TX_Pin, RX_Pin;
+  int BaudRate;
 };

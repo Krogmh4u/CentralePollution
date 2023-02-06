c830142e02755febdb6b39642a4fcc4458159960
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-02-06 13:58:58 +0100

    énorme con

--- Arduino_E3/communication.h
+++ Arduino_E3/communication.h
@@ -1,21 +1,12 @@
-class Communication : public SoftwareSerial
+#include <WString.h>
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+class Communication 
 {
   public :
-
-  Communication(const int TX_PIN, const int RX_PIN, const int BAUD_RATE = 9600) :
-    SoftwareSerial(RX_PIN, TX_PIN), 
-    TX_Pin(TX_PIN), RX_Pin(RX_PIN), BaudRate(BAUD_RATE)
-    {
-      begin(BaudRate);
-    }
-
     template<typename T> inline void SendData(const String name, T data) const noexcept
     {
-      Serial.println(name + ":" + String(data));
+      Serial.println("{" + name + "}" + "[" + String(data) + "]");
     }
-
-  private : 
-
-  int TX_Pin, RX_Pin;
-  int BaudRate;
 };

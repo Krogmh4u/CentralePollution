53e20cec751344e0e1ace0a530adb85038e1b8ce
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-02-06 13:58:34 +0100

    grosse connerie

--- Arduino_E3/test.ino
+++ Arduino_E3/test.ino
@@ -2,8 +2,6 @@
 #include "AirQualitySensor.h"
 #include "co2covSensor.h"
 
-#define USING_SOFTWARE_SERIAL //Retirer cette ligne pour debug avec le moniteur série
-
 // CAPTEURS UTILISES
 #define AIR_QUALITY_SENSOR
 
@@ -12,36 +10,24 @@
   #define AIR_QUALITY_SENSOR_PIN A0
 #endif
 
-#ifdef USING_SOFTWARE_SERIAL
-  #define TX_PIN 10
-  #define RX_PIN 11
-  Communication* communication;
-#endif 
-
+#define BAUD_RATE 9600
+  
+Communication* communication;
 AQSensor *airQualitySensor;
 Co2VocSensor *co2covSensor;
 
 void setup() {
-  
-  #ifdef USING_SOFTWARE_SERIAL
-    communication = new Communication(TX_PIN, RX_PIN, BAUD_RATE);
-  #else 
-    Serial.begin(BAUD_RATE)
-  #endif 
+    Serial.begin(BAUD_RATE);
+    communication = new Communication();
       
-  airQualitySensor = new AQSensor(AIR_QUALITY_SENSOR_PIN);
-  co2covSensor = new Co2VocSensor();
+    airQualitySensor = new AQSensor(AIR_QUALITY_SENSOR_PIN);
+    co2covSensor = new Co2VocSensor();
 }
 
 void loop() {
   delay(500);
-  #ifdef USING_SOFTWARE_SERIAL
-    communication->SendData("airquality", airQualitySensor->getValue());
-    communication->SendData("CO2", co2covSensor->GetCO2());
-    communication->SendData("COV", co2covSensor->GetCOV());
-  #else
-    Serial.println("airquality: %d", airQualitySensor->getValue());
-    Serial.println("CO2: %d", co2covSensor->GetCO2());
-    Serial.println("COV: %d", co2covSensor->GetCOV());
-  #endif
+  
+  communication->SendData("airquality", airQualitySensor->getValue());
+  communication->SendData("CO2", co2covSensor->GetCO2());
+  communication->SendData("COV", co2covSensor->GetCOV());
 }

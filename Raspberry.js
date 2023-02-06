65a625150e511cf8c0df400f6cedec4ecece7abc
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-02-06 13:58:15 +0100

    tout a changé

--- Raspberry.js
+++ Raspberry.js
@@ -15,15 +15,11 @@ if (err) throw err;
 console.log("[+] Connected to database");
 });
 
-var AirQuality = 0;
-var CO2 = 0;
-var COV = 0;
-var Temp = 0;
 
 const comPort1 = new SerialPort({
     path: 'COM4',
     parser: new ReadlineParser('\n'),
-    baudRate: 230400,
+    baudRate: 9600,
     dataBits: 8,
     stopBits: 1,
     parity: 'none',
@@ -31,13 +27,7 @@ const comPort1 = new SerialPort({
 
 function SendData(type, value)
 {
-    let valuestr = '';
-    for (var i = 0; i < value.toString().length; i++) {
-        if(value[i] != '\n' && value[i] != '\r' && value[i] != '\n\r')
-          valuestr+= value[i];
-    }
-    let sql = "INSERT INTO mesures (Capteur, Mesure) VALUES('" + type.toString() + "', '" + valuestr + "')";
-    console.log(sql);
+    let sql = "INSERT INTO mesures (Capteur, Mesure) VALUES('" + type.toString() + "', '" + value.toString() + "')";
     con.query(sql, function (err, result) {
         if (err) console.log("[-] " + err);
     });
@@ -45,47 +35,35 @@ function SendData(type, value)
 
 comPort1.on('data', data => readSerialData(data.toString()));
 
-function readSerialData(data) {
-    data.replace(/(\r\n|\n|\r)/gm, "");
-    if (data.indexOf(':') > -1)
-    {
-        var splitData = data.toString().split(":");
-        if(splitData.length == 2)
-        {
-            switch(splitData[0])
-            {
-                case 'airquality' :
-                    if(Number(splitData[1].toString()) >= 0 && Number(splitData[1].toString()) <= 3){
-                        AirQuality = Number(splitData[1].toString());
-                        SendData(splitData[0], splitData[1]);
-                    }
-                    break;
-                case 'CO2':
-                    if(Number(splitData[1].toString()) >= 0 && Number(splitData[1].toString()) <= 20000){
-                        CO2 = Number(splitData[1].toString());
-                        SendData(splitData[0], splitData[1]);
-                    }
-                    break;
-                case 'COV':
-                    if(Number(splitData[1].toString()) >= 0.0 && Number(splitData[1].toString()) <= 50.0){
-                        COV = parseFloat(splitData[1].toString());
-                        SendData(splitData[0], splitData[1]);
-                    }
-                    break;
-                case 'temperature':
-                    if(parseFloat(splitData[1].toString()) >= 0.0 && parseFloat(splitData[1].toString()) <= 50.0){
-                        Temp = parseFloat(splitData[1].toString());
-                        SendData(splitData[0], splitData[1]);
-                    }
-                    break;
-            }
-        } 
-    } 
 
-    /*console.log("Air Quality : " + AirQuality.toString());
-    console.log("CO2 : " + CO2.toString() + "ppm");
-    console.log("COV : " + COV.toString() + "ppb");
-    console.log("Temperature : " + Temp.toString() + "°C");
-    console.log("====================================");
-   */
+//enum statements
+const BEGIN_NAME = 0;
+const END_NAME = 1;
+const BEGIN_VALUE = 2;
+const END_VALUE = 3;
+const UNKNOWN_STMT = 4;
+
+var currentDataName = "";
+var currentDataValueStr = "";
+
+var currentStatement = UNKNOWN_STMT;
+
+function readSerialData(data) {
+    for (var i = 0; i < data.toString().length; i++) {
+        switch(data[i]){
+            case '{' : currentStatement = BEGIN_NAME; break;
+            case '}' : currentStatement = END_NAME; break;
+            case '[' : currentStatement = BEGIN_VALUE; break;
+            case ']' : currentStatement = END_VALUE; break;
+            default : 
+                if(currentStatement == BEGIN_NAME) currentDataName+= data[i];
+                else if(currentStatement == BEGIN_VALUE) currentDataValueStr += data[i];
+                else if(currentStatement == END_VALUE){
+                    SendData(currentDataName, number(currentDataValueStr));
+                    currentDataName = "";
+                    currentDataValueStr ="";
+                }
+            break;
+        }
+    }
 }
